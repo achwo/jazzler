07afca50e73fd10cb7c02486e9214795f496316a
Felix Jensen <engineduck@posteo.de>
2015-09-16 16:49:17 +0200

    Initial clojure project.clj with midje and auto import of midje in repl

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject jazzler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main ^:skip-aot jazzler.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.7.0"]]
+            :main ^:skip-aot jazzler.core
+            :target-path "target/%s"
+            :profiles {:uberjar {:aot :all}
+                       :dev {:dependencies [[midje "1.7.0"]]
+                             :plugins [[lein-midje "3.1.3"]]
+                             :repl {:dependencies [[midje "1.7.0"]]}
+                             :repl-options {:init (use 'midje.repl)}}})
