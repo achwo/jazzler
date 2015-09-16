7c7a3ac9e87be195406dfa456929297d12989f52
Felix Jensen <engineduck@posteo.de>
2015-09-16 16:47:32 +0200

    leiningen initial files, README and LICENSE

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jazzler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jazzler/core_test.clj
@@ -0,0 +1,7 @@
+(ns jazzler.core-test
+  (:require [clojure.test :refer :all]
+            [jazzler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
