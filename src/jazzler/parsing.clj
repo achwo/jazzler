41c9e96c24a058a301a0d8f293351519d099963f
Felix Jensen <engineduck@posteo.de>
2015-10-20 12:47:30 +0200

    implemented basic progression parsing: major chords, bars with multiple chords and chords as bar of only this chord work

--- src/jazzler/parsing.clj
+++ src/jazzler/parsing.clj
@@ -1,2 +1,39 @@
 (ns jazzler.parsing
-  (:gen-class))
+  (:gen-class)
+  (:require [instaparse.core :as i]))
+
+(defn progression 
+  [& content]
+  (cond
+    (seq? content) [:progression content]
+    (nil? content) [:progression '()]
+    :else [:progression (list content)];unused
+))
+
+(defn bar [& content]
+  [:bar content])
+
+(defn barchord [content]
+  [:bar (list content)])
+
+(def progression-parser
+  (i/parser 
+   (str
+    "progression = <'['>barOrChord? (<ws> barOrChord)* <']'> "
+    "<barOrChord> = bar | bchord "
+    "bar = <'['> chord (<ws> chord)* <']'> "
+    "bchord = chord "
+    "chord = 'I' | 'II' | 'III' | 'IV' | 'V' |'VI' | 'VII' "
+    "ws = #'\\s+'")))
+
+(def transformations
+  {:progression progression
+   :bchord barchord
+   :chord str
+   :bar bar})
+
+(defn parse-progression [string]
+  (->> string
+       (progression-parser)
+       (i/transform transformations)))
+
--- test/jazzler/parsing_test.clj
+++ test/jazzler/parsing_test.clj
@@ -3,21 +3,25 @@
   (:require [jazzler.parsing :refer :all]
             [instaparse.core :as i]))
 
-(def progression-parser
-  (i/parser
-"progression = <'['>bar?<']'>
-bar = <'['>chord<']'>
-chord = #'[A-G]'
-whitespace = #'\\s+'"))
-
-(defn parse-progression [string]
-  (->> (progression-parser string)
-       ))
-
-(facts "about parse-progression"
+(facts "about parse progression"
   (fact "[] returns empty progression"
-    (parse-progression "[]") => [:progression])
-  (fact "[[C]] is one bar with one chord"
-    (parse-progression "[[C]]") => [:progression [:bar [:chord "C"]]]))
-
-;; FIXME more accurat chord definition
+    (parse-progression "[]") => [:progression '()])
+  (fact "[[I]] is one bar with one chord"
+    (parse-progression "[[I]]") 
+    => [:progression (list [:bar (list "I")])])
+  (fact "[I] is one bar of I"
+    (parse-progression "[I]") 
+    => [:progression (list [:bar (list "I")])])
+  (fact "[[I IV]] is one bar of two chords"
+    (parse-progression "[[I IV]]") 
+    => [:progression (list [:bar (list "I" "IV")])])
+  (fact "[[I] [IV]] is one bar of each chord"
+    (parse-progression "[[I] [IV]]") 
+    => [:progression (list [:bar (list "I")]
+                           [:bar (list "IV")])])
+  (fact "[I IV] is one bar of each chord"
+               (parse-progression "[I IV]") 
+               => [:progression (list [:bar (list "I")]
+                                      [:bar (list "IV")])])
+  
+)
