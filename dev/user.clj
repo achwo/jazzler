378ccfdef6176ced635f8f9f4393174d793146e8
Felix Jensen <engineduck@posteo.de>
2015-12-01 16:35:32 +0100

    add tools.namespace to project and user ns

--- dev/user.clj
+++ dev/user.clj
@@ -2,6 +2,7 @@
   (:require [clojure.repl :refer [doc source]]
             [clojure.pprint :refer [pprint]]
             [midje.repl :refer :all]
+            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [jazzler.song :refer :all]
             [jazzler.repl :refer :all]
             [jazzler.repl.system :as sys]
--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject jazzler "0.1.0-SNAPSHOT"
-            :description "a language for jazz music composition"
-            :url "https://github.com/engineduck/jazzler"
-            :license {:name "GNU General Public License, V3.0"
-                      :url "http://www.gnu.org/licenses/gpl.html"}
-            :dependencies [[org.clojure/clojure "1.7.0"]
-                           [instaparse "1.4.1"]
-                           [quantisan/overtone "0.10-SNAPSHOT"]]
-            :main ^:skip-aot jazzler.repl
-            :target-path "target/%s"
-            :profiles {:uberjar {:aot :all}
-                       :dev {:source-paths ["dev"]
-                             :dependencies [[midje "1.8.2"]
-                                            [rhizome "0.2.5"]]
-                             :plugins [[lein-midje "3.2"]]
-                             }})
+  :description "a language for jazz music composition"
+  :url "https://github.com/engineduck/jazzler"
+  :license {:name "GNU General Public License, V3.0"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [instaparse "1.4.1"]
+                 [quantisan/overtone "0.10-SNAPSHOT"]]
+  :main ^:skip-aot jazzler.repl
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:source-paths ["dev"]
+                   :dependencies [[midje "1.8.2"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [rhizome "0.2.5"]]
+                   :plugins [[lein-midje "3.2"]]}})
